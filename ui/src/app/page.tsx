75be28567f7f34445a6c587fe024a5394221cf29
Alisina <alisinasoltani82@gmail.com>
2024-11-30 03:14:16 +0330

    ui!: industries component created

--- ui/src/app/page.tsx
+++ ui/src/app/page.tsx
@@ -1,11 +1,13 @@
 import { Navbar } from "@/components/Navbar";
 import Slider from "@/components/Slider";
+import Industries from "@/components/Industries";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <Slider />
+      <Industries />
     </>
   );
 }
