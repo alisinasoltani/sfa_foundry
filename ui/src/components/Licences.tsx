7a83f2217ac010e88565708d1c24dfa5a8543d31
Alisina <alisinasoltani82@gmail.com>
2024-12-29 03:19:39 +0330

    ui: licences responsive complete

--- ui/src/components/Licences.tsx
+++ ui/src/components/Licences.tsx
@@ -8,17 +8,17 @@ import licence3 from "@/public/licences/licence3.png";
 const Licences = () => {
   return (
     <>
-      <div className="w-full relative bg-[#FDFDFD] -translate-y-10 z-0 flex flex-col xl:gap-12 gap-6 pt-12 justify-center items-center xl:h-[100vh] lg:h-[80vh] h-[150vh]">
+      <div className="w-full relative bg-[#FDFDFD] -translate-y-10 z-0 flex flex-col xl:gap-12 gap-6 pt-12 justify-center items-center lg:h-[100vh] md:h-[80vh] h-[120vh]">
           <div className="xl:flex hidden z-10 absolute top-4 left-0 p-0 m-0 w-full h-[100vh]">
             <Image src={waves} height={600} alt="" />
           </div>
           <div className="z-0 absolute xl:top-[48vh] lg:top-[40vh] top-[110vh] left-0 p-0 m-0 overflow-hidden w-full xl:h-[130vh] lg:h-[100vh] h-[50vh]">
             <Image src={map} priority fill className="z-0" alt="" />
           </div>
-          <h2 className="sf_bold xl:text-[2rem] text-2xl text-center z-10">
+          <h2 className="sf_bold lg:text-[2rem] md:text-2xl sm:text-[1.2rem] text-center z-10">
               قابل اعتماد و تایید شده
           </h2>
-          <span className="sf_semi xl:text-3xl text-xl text-center lg:max-w-[45vw] xl:p-0 px-8 z-10" style={{ direction: 'rtl' }}>
+          <span className="sf_semi lg:text-3xl md:text-xl sm:text-[1rem] text-center lg:max-w-[45vw] xl:p-0 px-8 z-10" style={{ direction: 'rtl' }}>
           شرکت سپاهان فولاد آتشگاه، دارای گواهینامه های ISO 14001:2004 و ISO 9001:2008 است.
           کلیه مراحل تولید در این شرکت طبق دستورالعمل های سیستم کنترل کیفیت انجام می شود.
           </span>
