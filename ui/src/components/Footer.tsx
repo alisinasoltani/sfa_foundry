5dc73be2c6b229437f7255f2107911f0f11010ba
Alisina <alisinasoltani82@gmail.com>
2024-12-29 03:16:35 +0330

    ui: footer responsive complete

--- ui/src/components/Footer.tsx
+++ ui/src/components/Footer.tsx
@@ -10,7 +10,7 @@ import mailIcon from "@/public/icons/mail.svg";
 
 const Footer = () => {
   return (
-    <div className="flex lg:flex-row flex-col-reverse lg:gap-0 gap-12 justify-center items-start bg-none lg:pt-[12vh] w-full xl:h-[60vh] lg:h-[70vh] h-[140vh] relative z-[100]">
+    <div className="flex lg:flex-row flex-col-reverse lg:gap-0 gap-12 justify-center items-start bg-none lg:pt-[12vh] w-full xl:h-[60vh] lg:h-[70vh] h-[120vh] relative z-[100]">
         <div className="flex flex-col justify-center items-center lg:gap-4 gap-8 w-full xl:px-[4rem] px-[2rem]">
             <h2 className="lg:text-3xl text-2xl sf_bold border-b-2 border-[#D52927] pb-3 text-center w-full" style={{ direction: 'rtl' }}>
                 ارتباط با ما
@@ -36,18 +36,18 @@ const Footer = () => {
                 <div className="w-full flex flex-row-reverse text-right gap-4 justify-center items-center">
                     <Image src={icon} alt="sfa icon" width={80} />
                     <div className="flex flex-col">
-                        <h6 className="xl:text-[2rem] text-[1.8rem] sf_light">
+                        <h6 className="xl:text-[2rem] text-[1.8rem] sm:text-[1rem] sf_light">
                             صنایع ریخته گری
                         </h6>
-                        <h6 className="xl:text-[1.8rem] text-[1.5rem] sf_medium">
+                        <h6 className="xl:text-[1.8rem] text-[1.5rem] sm:text-[0.9rem] sf_medium">
                             سپاهان فولاد آتشگاه
                         </h6>
                     </div>
                 </div>
-                <h6 className="lg:w-[22rem] lg:text-[1.5rem] text-[1.2rem] sf_medium text-center" style={{ direction: 'rtl' }}>
+                <h6 className="lg:w-[22rem] lg:text-[1.5rem] text-[1.2rem] sm:text-[1rem] sf_medium text-center max-w-[358px]" style={{ direction: 'rtl' }}>
                     کلیه حقوق مادی و معنوی، متعلق به سپاهان فولاد آتشگاه می باشد.
                 </h6>
-                <h6 className="lg:w-[22rem] text-center text-[1.2rem] sf_semi" style={{ direction: 'rtl' }}>
+                <h6 className="lg:w-[22rem] text-center text-[1.2rem] sm:text-[0.9rem] sf_semi" style={{ direction: 'rtl' }}>
                     طراحی شده توسط تیم آکادمی پنج
                 </h6>
             </div>
@@ -56,24 +56,24 @@ const Footer = () => {
             <h2 className="lg:text-3xl text-2xl sf_bold border-b-2 border-[#D52927] pb-3 text-center w-full" style={{ direction: 'rtl' }}>
                 تماس با ما
             </h2>
-            <div className="flex flex-row justify-end gap-2 lg:text-3xl text-2xl sf_light">
+            <div className="flex flex-row justify-end gap-2 lg:text-3xl text-2xl sm:text-[1.1rem] sf_light">
                 031-42696500-4
                 <Image src={phoneIcone} width={25} alt="phone number" />
             </div>
-            <div className="flex flex-row justify-end gap-2 lg:text-3xl text-2xl sf_light">
+            <div className="flex flex-row justify-end gap-2 lg:text-3xl text-2xl sm:text-[1.1rem] sf_light">
                 031-42696505
                 <Image src={faxIcone} width={25} alt="fax number" />
             </div>
             <div className="flex flex-col justify-center items-end gap-4">
-                <div className="flex flex-row justify-end gap-2 lg:text-3xl text-xl sf_light">
+                <div className="flex flex-row justify-end gap-2 lg:text-3xl text-xl sm:text-[1.1rem] sf_light">
                     info@sfa-foundry.com
                     <Image src={mailIcon} width={25} alt="fax number" />
                 </div>
-                <div className="flex flex-row justify-end gap-2 lg:text-3xl text-xl sf_light">
+                <div className="flex flex-row justify-end gap-2 lg:text-3xl text-xl sm:text-[1.1rem] sf_light">
                     sales@sfa-foundry.com
                     <Image src={mailIcon} width={25} alt="fax number" />
                 </div>
-                <div className="flex flex-row justify-end gap-2 lg:text-3xl text-[1.20rem] sf_light">
+                <div className="flex flex-row justify-end gap-2 lg:text-3xl text-[1.20rem] sm:text-[1.1rem] sf_light">
                     export@sfa-foundry.com
                     <Image src={mailIcon} width={25} alt="fax number" />
                 </div>
@@ -83,7 +83,7 @@ const Footer = () => {
             <h2 className="lg:text-3xl text-2xl sf_bold border-b-2 border-[#D52927] pb-3 text-center w-full" style={{ direction: 'rtl' }}>
                 آدرس ما
             </h2>
-            <h5 className="xl:text-3xl text-2xl sf_medium lg:w-[18vw] w-full" style={{ direction: 'rtl', lineHeight: '2.4rem' }}>
+            <h5 className="xl:text-3xl text-2xl sm:text-[1.1rem] sf_medium lg:w-[18vw] w-full" style={{ direction: 'rtl', lineHeight: '2.4rem' }}>
                 اصفهان، شهرک صنعتی نجف آباد ۲، میدان صنعت،  بلوار دکتر حسابی، نبش فرعی۲۸ 
             </h5>
         </div>
